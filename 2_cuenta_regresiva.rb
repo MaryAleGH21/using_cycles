0e5f5c4ac2e347c669c0f243d1cea79e4b448700
Alejandr Serrano <marialesr.17@gmail.com>
2021-02-01 11:21:11 -0600

    first three exercise

---
+++ 2_cuenta_regresiva.rb
@@ -0,0 +1,15 @@
+
+=begin
+until cuenta_regresiva < 0
+puts cuenta_regresiva
+cuenta_regresiva -= 1
+end
+=end
+
+cuenta_regresiva = ARGV[0].to_i
+
+while cuenta_regresiva >= 0
+    puts "Contando desde #{cuenta_regresiva}..."
+    cuenta_regresiva -= 1
+    sleep 0.5
+end   
---
+++ 3_ruby_solo_pares.rb
@@ -0,0 +1,7 @@
+number = ARGV[0].to_i
+
+number.times do |i|
+  puts "#{i * 2}"  
+end
+
+number.times {|i| puts "#{i * 2}"}
